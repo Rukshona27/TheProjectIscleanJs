32cd6760f0bd956949388bb4b13caf0faf4b2cb7
Rukshona27 <rukshona.lu@gmail.com>
2024-05-07 17:37:58 +0300

    the project is clean js

---
+++ client/js/index.js
@@ -0,0 +1,27 @@
+import { createClientsHeader } from "./createHeader.js";
+import { createClientsSection } from "./createClientsSection.js";
+import { getClients } from "./clientsApi.js";
+import { createClientItem } from "./createClientItem.js";
+
+const createApp = async () => {
+    const clients = await getClients();
+    const header = createClientsHeader();
+    const clientSection = createClientsSection();
+    document.body.append(header, clientSection.main);
+
+    for (const client of clients) {
+        document.querySelector('.clients__tbody').append(createClientItem(client));
+    }
+
+    // window.onload = () => {
+    //     const preloader = document.querySelector('.preloader');
+    //     preloader.remove();
+        
+    //     for (const client of clients) {
+    //         document.querySelector('.clients__tbody').append(createClientItem(client));
+    //     }
+    // };
+    
+}
+
+createApp();
